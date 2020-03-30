dc7180ae5aa36042d144519cf0930cc7fddc9a7a
Anshul Dahiya <anshul.rts@gmail.com>
2020-03-30 21:58:15 +0530

    Adding DbContext class

--- EmployeeManagementNetCore/Startup.cs
+++ EmployeeManagementNetCore/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeManagementNetCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +25,10 @@ namespace EmployeeManagementNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string abc = _config.GetConnectionString("EmployeeDBConnection");
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+
             services.AddMvc();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
