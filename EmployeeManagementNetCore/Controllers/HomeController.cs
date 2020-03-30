7d7e2a763fd416c0a016ea8d6d4405350527c3dc
Anshul Dahiya <anshul.rts@gmail.com>
2020-03-30 14:54:43 +0530

    Form Tag helper and Model Binding

--- EmployeeManagementNetCore/Controllers/HomeController.cs
+++ EmployeeManagementNetCore/Controllers/HomeController.cs
@@ -33,5 +33,18 @@ namespace EmployeeManagementNetCore.Controllers
 
             return View(homeDetailsViewModel);
         }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Employee employee)
+        {
+            Employee emp = _employeeRepository.Add(employee);
+            return RedirectToAction("Details", new { id = emp.Id });
+        }
     }
 }
--- EmployeeManagementNetCore/Models/IEmployeeRepository.cs
+++ EmployeeManagementNetCore/Models/IEmployeeRepository.cs
@@ -10,5 +10,7 @@ namespace EmployeeManagementNetCore.Models
         Employee GetEmployee(int Id);
 
         IEnumerable<Employee> GetAllEmployees();
+
+        Employee Add(Employee employee);
     }
 }
