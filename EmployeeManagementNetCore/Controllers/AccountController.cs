34c8ad01cca0380245b73824b2e5e6573ff994fd
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-03 13:01:29 +0530

    Implemented Login Functionality

--- EmployeeManagementNetCore/Controllers/AccountController.cs
+++ EmployeeManagementNetCore/Controllers/AccountController.cs
@@ -50,6 +50,31 @@ namespace EmployeeManagementNetCore.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,
+                                                                     model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid login credentials");
+            }
+
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
