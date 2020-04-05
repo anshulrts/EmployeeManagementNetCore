fd230d176dfd4300d9a44003cfe9dd5e51531d6c
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-05 12:00:37 +0530

    Displaying list of all roles

--- EmployeeManagementNetCore/Controllers/AdministrationController.cs
+++ EmployeeManagementNetCore/Controllers/AdministrationController.cs
@@ -37,7 +37,7 @@ namespace EmployeeManagementNetCore.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    return RedirectToAction("ListRoles", "Administration");
                 }
                 
                 foreach (IdentityError error in result.Errors)
@@ -46,6 +46,13 @@ namespace EmployeeManagementNetCore.Controllers
                 }
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
         }
     }
 }
