7619c19c84b1babd78604ed3116d1ac33f4279e7
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-08 11:30:53 +0530

    Better way of confirming delete

--- EmployeeManagementNetCore/wwwroot/js/CustomScript.js
+++ EmployeeManagementNetCore/wwwroot/js/CustomScript.js
@@ -1,1 +1,14 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    deleteSpan = "deleteSpan_" + uniqueId;
+    confirmDeleteSpan = "confirmDeleteSpan_" + uniqueId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    }
+    else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+
+}
